59c58ad0b8d7f3de34e7498253f58de966322801
Enrique Morell <emorell96@hotmail.com>
2019-04-17 12:25:24 +0200

    frequency class updated with auto unit

--- frequency.py
+++ frequency.py
@@ -1,5 +1,5 @@
 class FreqUnit:
-    MHz, GHz, kHz = range(1,4)
+    Hz, kHz, MHz, GHz = [(i, 1000**i) for i in range(4)]
     def __init__(self, Type):
         self.value = Type
     def __str__(self):
@@ -11,10 +11,22 @@ class FreqUnit:
             return "kHz"
     def __eq__(self, y):
         return self.value == y.value
+    
 class Frequency:
+    __rmul__ = __mul__
     def __init__(self, value, unit: FreqUnit, time = None):
         self.time = time
         self.value = value
         self.unit = unit
     def __str__(self):
         return "{0:.1f} {1}".format(self.value, self.unit)
+    def convert(self, targetunit: FreqUnit):
+        coeff1 = self.unit[1]
+        coeff2 = targetunit.value[1]
+        if coeff1 != coeff2:
+            return Frequency(self.value*coeff1/coeff2, targetunit, self.time)        
+    def __mul__(self, other, unit_size = None):
+        if self.unit == other.unit:
+            return Frequency(self.value*other.value, self.unit, self.time)
+        return self.convert(FreqUnit(FreqUnit.Hz))*self.convert(FreqUnit(FreqUnit.Hz))
+        
