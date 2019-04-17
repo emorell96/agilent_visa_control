5d2d437213ccccbe3143fe57ab7a7360f78fd546
Enrique Morell <emorell96@hotmail.com>
2019-04-17 18:42:37 +0200

    added license to the setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agilent-visa-control",
-    version="0.0.2.1.1",
+    version="0.0.2.1.2",
     author="Enrique Morell",
     author_email="morell.enrique@outlook.com",
     description="A small package aimed at helping with interfacing with Agilent spectrum analyzer (tested with ESA and EXA series)",
@@ -18,5 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    install_requires=['PyVISA>=1.9']
+    install_requires=["PyVISA>=1.9"],
+    license = "GNU General Public License v3 (GPLv3)",
+    data_files = [("", ["LICENSE"])]
 )
